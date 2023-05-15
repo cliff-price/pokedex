6f03bc5d8a6528f7e8f56b7269d260321072a52d
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-05-15 17:12:30 -0500

    Update scripts.js
    Create a new variable called pokemonList and assign to it a blank array.  Add several Poekmon objects with multiple keys and multiple values.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [null];
+PokemonList=[
+    {name:"Bulbasaur",height:7,type:["grass","poison"]},
+    {name:"Eve",height:22,type:["water","speed"]},
+    {name:"Idunno", height:99,type:["gotme","gotyou"]}];

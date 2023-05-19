0c9b653e5a127aa7539473ab0b57fa48cca951b5
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-05-19 13:41:18 -0500

    Update scripts.js
    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,4 @@ pokemonList=[
     {name:"Bulbasaur",height:7,type:["grass","poison"]},
     {name:"Eve",height:22,type:["water","speed"]},
     {name:"Idunno", height:99,type:["gotme","gotyou"]}];
+// I am not famliar with Pokeman so I just made up names and characteristics

6ea47d3b71696c0d2176a4b90c4bb9943590c4ff
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-05-19 14:27:28 -0500

    Update scripts.js
    Displayed a list of Pokemon objects, and their height, to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ pokemonList=[
     {name:"Eve",height:22,type:["water","speed"]},
     {name:"Idunno", height:99,type:["gotme","gotyou"]}];
 // I am not famliar with Pokeman so I just made up names and characteristics
+
+for (let i = 0; i < pokemonList.length; i++) {
+    console.log(pokemonList[i].name + " height = " +pokemonList[i].height);
+    document.write(pokemonList[i].name + " height = " +pokemonList[i].height + "<br>")   
+}
+// added a list of Pokemon objects to the DOM along with their height
+// included a line break for visual appearance
+// Works in Edge, but error when I try to run on Chrome: "This document requires 'TrustedHTML' assignment."

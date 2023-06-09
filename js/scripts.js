b48f8fc9204125eab31f9efe5064872fb9e25567
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-06-09 16:47:59 -0500

    Made pokemonList an IIFE
    Made pokemonList an IIFE, initialized it and added objects via the add function.  Used getAll to retrieve objects from the repository an updated rest of code accordingly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,45 @@
-let pokemonList = [];
-pokemonList=[
-    {name:"Bulbasaur",height:7,type:["grass","poison"]},
-    {name:"Eve",height:22,type:["water","speed"]},
-    {name:"Idunno", height:99,type:["gotme","gotyou"]}];
-// I am not famliar with Pokeman so I just made up names and characteristics
+let pokemonRepository = (function () {
+    let pokemonListInternal = [];
 
-/*for (let i = 0; i < pokemonList.length; i++) {
+    function add(pokemon) {
+      pokemonListInternal.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonListInternal;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+  console.log(pokemonRepository.getAll());
+
+  pokemonRepository.add({name:"Bulbasaur",height:7,type:["grass","poison"]});
+  pokemonRepository.add({name:"Eve",height:22,type:["water","speed"]});
+  pokemonRepository.add({name:"Idunno", height:99,type:["gotme","gotyou"]});
+  // I am not famliar with Pokeman so I just made up names and characteristics>
+
+console.log(pokemonRepository.getAll());
+
+let pokemonListExternal = [];
+pokemonListExternal = [pokemonRepository.getAll()];
+console.log(pokemonListExternal);
+
+/*OLD CODE:
+for (let i = 0; i < pokemonList.length; i++) {
     console.log(pokemonList[i].name + " height = " +pokemonList[i].height);
     document.write(pokemonList[i].name + " height = " +pokemonList[i].height);
     if (pokemonList[i].height > 30)
     {document.write(" - Wow, that's big!")};
     document.write("<br>")  */
-    pokemonList.forEach(function(user) {
-        console.log(user.name + " height = " +user.height);
+
+// pokemonListExternal.forEach(function(user) {
+    //  Why doesn't pokemonListExternal work?  It shows up as an Arry wusing console.log(), but the va;lues are undefined inteh forEACDH function.
+pokemonRepository.getAll().forEach(function(user) {
+    console.log(user.name + " height = " +user.height);
     document.write(user.name + " height = " +user.height);
     if (user.height > 30)
     {document.write(" - Wow, that's big!")};
@@ -21,4 +48,4 @@ pokemonList=[
 // added a list of Pokemon objects to the DOM along with their height
 // included a line break for visual appearance
 // Works in Edge, but error when I try to run on Chrome: "This document requires 'TrustedHTML' assignment."
-// added a condition to check for height above 30 and print a message tot he DOM
+// added a condition to check for height above 30 and print a message to the DOM

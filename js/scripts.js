ca7fb4dfc3f96f4afa47ea9f58a6b8705d4f4201
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-06-09 13:56:27 -0500

    Replace for loop with forEach function
    Replace for loop with forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,19 @@ pokemonList=[
     {name:"Idunno", height:99,type:["gotme","gotyou"]}];
 // I am not famliar with Pokeman so I just made up names and characteristics
 
-for (let i = 0; i < pokemonList.length; i++) {
+/*for (let i = 0; i < pokemonList.length; i++) {
     console.log(pokemonList[i].name + " height = " +pokemonList[i].height);
     document.write(pokemonList[i].name + " height = " +pokemonList[i].height);
     if (pokemonList[i].height > 30)
     {document.write(" - Wow, that's big!")};
-    document.write("<br>")  
-}
+    document.write("<br>")  */
+    pokemonList.forEach(function(user) {
+        console.log(user.name + " height = " +user.height);
+    document.write(user.name + " height = " +user.height);
+    if (user.height > 30)
+    {document.write(" - Wow, that's big!")};
+    document.write("<br>");
+});
 // added a list of Pokemon objects to the DOM along with their height
 // included a line break for visual appearance
 // Works in Edge, but error when I try to run on Chrome: "This document requires 'TrustedHTML' assignment."

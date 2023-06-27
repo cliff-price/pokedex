043c866ccccc0fc3b8597881faa650ff36170ad7
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-06-27 16:20:29 -0500

    working document
    working document submitted for guidance

--- js/scripts.js
+++ js/scripts.js
@@ -15,18 +15,13 @@ let pokemonRepository = (function () {
     };
   })();
 
-  console.log(pokemonRepository.getAll());
-
   pokemonRepository.add({name:"Bulbasaur",height:7,type:["grass","poison"]});
   pokemonRepository.add({name:"Eve",height:22,type:["water","speed"]});
   pokemonRepository.add({name:"Idunno", height:99,type:["gotme","gotyou"]});
   // I am not famliar with Pokeman so I just made up names and characteristics>
 
-console.log(pokemonRepository.getAll());
-
 let pokemonListExternal = [];
 pokemonListExternal = pokemonRepository.getAll();
-console.log(pokemonListExternal);
 
 /*OLD CODE:
 for (let i = 0; i < pokemonList.length; i++) {
@@ -37,17 +32,22 @@ for (let i = 0; i < pokemonList.length; i++) {
     document.write("<br>")  */
 
 pokemonListExternal.forEach(function(user) {
-    //  Why doesn't pokemonListExternal work?  It shows up as an Arry wusing console.log(), but the va;lues are undefined inteh forEACDH function.
+    //  pokemonListExternal is interchangeable in teh forEach loop with pokemonRepository.gatAll()
 //pokemonRepository.getAll().forEach(function(user) {
-    console.log(user.name + " height = " +user.height);
-    document.write(user.name + " height = " +user.height);
-    if (user.height > 30)
-    {document.write(" - Wow, that's big!")};
-    document.write("<br>");
+let pokemonList = document.querySelector('.pokemon-list'); 
+//console.log(pokemonList);
+let listItem = document.createElement('li');
+let button = document.createElement('button');
+button.innerText = user.name;
+console.log(user.name);
+button.classList.add('buttonItem');
+listItem.appendChild(button);
+console.log(button);
+console.log(listItem);
+pokemonList.appendChild(listItem);
 });
 
 console.log(pokemonListExternal);
 // added a list of Pokemon objects to the DOM along with their height
 // included a line break for visual appearance
-// Works in Edge, but error when I try to run on Chrome: "This document requires 'TrustedHTML' assignment."
 // added a condition to check for height above 30 and print a message to the DOM

a4c080dd446b10956298285dfe5cda43c80a37b9
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-06-28 16:52:58 -0500

    Update Code to Manipulate DOM and Moved Code into IIFE
    Update Code to Manipulate DOM and Moved Code into IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,39 @@
 let pokemonRepository = (function () {
-    let pokemonListInternal = [];
+let pokemonListInternal = [];
 
     function add(pokemon) {
       pokemonListInternal.push(pokemon);
     }
-  
+
+    function addListItem(pokemon) {
+      let pokemonList = document.querySelector('.pokemon-list'); 
+      let listItem = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('buttonItem');
+      listItem.appendChild(button);
+      pokemonList.appendChild(listItem);
+      }
+
+      
     function getAll() {
       return pokemonListInternal;
     }
   
     return {
       add: add,
-      getAll: getAll
-    };
-  })();
-
-  console.log(pokemonRepository.getAll());
+      getAll: getAll,
+      addListItem: addListItem
+    }})
+  ();
 
   pokemonRepository.add({name:"Bulbasaur",height:7,type:["grass","poison"]});
   pokemonRepository.add({name:"Eve",height:22,type:["water","speed"]});
   pokemonRepository.add({name:"Idunno", height:99,type:["gotme","gotyou"]});
   // I am not famliar with Pokeman so I just made up names and characteristics>
 
-console.log(pokemonRepository.getAll());
-
 let pokemonListExternal = [];
 pokemonListExternal = pokemonRepository.getAll();
-console.log(pokemonListExternal);
 
 /*OLD CODE:
 for (let i = 0; i < pokemonList.length; i++) {
@@ -36,18 +43,20 @@ for (let i = 0; i < pokemonList.length; i++) {
     {document.write(" - Wow, that's big!")};
     document.write("<br>")  */
 
-pokemonListExternal.forEach(function(user) {
-    //  Why doesn't pokemonListExternal work?  It shows up as an Arry wusing console.log(), but the va;lues are undefined inteh forEACDH function.
-//pokemonRepository.getAll().forEach(function(user) {
-    console.log(user.name + " height = " +user.height);
-    document.write(user.name + " height = " +user.height);
-    if (user.height > 30)
-    {document.write(" - Wow, that's big!")};
-    document.write("<br>");
+//pokemonListExternal.forEach(function(user) {
+    //  pokemonListExternal is interchangeable in the forEach loop with pokemonRepository.gatAll()
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
+//let pokemonList = document.querySelector('.pokemon-list'); 
+//let listItem = document.createElement('li');
+//let button = document.createElement('button');
+//button.innerText = user.name;
+//button.classList.add('buttonItem');
+//listItem.appendChild(button);
+//pokemonList.appendChild(listItem);
 });
 
 console.log(pokemonListExternal);
-// added a list of Pokemon objects to the DOM along with their height
+  // added a list of Pokemon objects to the DOM along with their height
 // included a line break for visual appearance
-// Works in Edge, but error when I try to run on Chrome: "This document requires 'TrustedHTML' assignment."
 // added a condition to check for height above 30 and print a message to the DOM

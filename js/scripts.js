8b04090dc1bfaf5e44e210e1240e1d71756b6e4f
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-07-06 17:41:36 -0500

    Implemented pokedex as a modal
    Implemented pokedex as a modal.  Used a promise to obtain pokemon data from a public API. using a promise to

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 let pokemonRepository = (function () {
-  let pokemonListInternal = [];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal-container');
   
       function add(pokemon) {
-        pokemonListInternal.push(pokemon);
+        pokemonList.push(pokemon);
       }
   
       function addListItem(pokemon) {
@@ -15,52 +17,112 @@ let pokemonRepository = (function () {
         pokemonList.appendChild(listItem);
         button.addEventListener('click', function(event){showDetails(pokemon)});
         }
-  
-        function showDetails(pokemon){
-          console.log(pokemon.name);
-        } 
-        
+
+        function loadList() {
+          return fetch(apiUrl).then(function (response) {
+            return response.json();
+          }).then(function (json) {
+            json.results.forEach(function (item) {
+              let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+              };
+              add(pokemon);
+            });
+          }).catch(function (e) {
+            console.error(e);
+          })
+        }
+          
       function getAll() {
-        return pokemonListInternal;
+        return pokemonList;
       }
     
-      return {
+      function showDetails(pokemon) {
+        loadDetails(pokemon).then(function () {
+          console.log(pokemon);
+                modalContainer.innerHTML = '';
+        let modal = document.createElement('div');
+          modal.classList.add('modal');
+
+          let closeButtonElement = document.createElement('button');
+          closeButtonElement.classList.add('modal-close');
+          closeButtonElement.innerText = 'Close';
+          closeButtonElement.addEventListener('click', hideModal);
+
+          let nameElement = document.createElement('h1');
+          nameElement.innerText = pokemon.name
+          let heightElement = document.createElement('h1');
+          heightElement.innerText = "height = " + pokemon.height;
+          let imageElement = document.createElement('img');
+          imageElement.src = pokemon.imageUrl;
+          imageElement.width = 250;
+          imageElement.height = 250;
+
+          console.log(nameElement)
+          console.log(heightElement);
+
+          modal.appendChild(closeButtonElement);
+          modal.appendChild(nameElement);
+          modal.appendChild(heightElement);
+          modal.appendChild(imageElement);
+          modalContainer.appendChild(modal);
+          
+          modalContainer.classList.add('is-visible');
+        });
+      }
+
+      function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then(function (details) {
+          // Now we add the details to the item
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e) {
+          console.error(e);
+        });
+      }
+
+      function hideModal() {
+        modalContainer.classList.remove('is-visible');
+      }  
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+  
+  modalContainer.addEventListener('click', (e) => {
+     //Since this is also triggered when clicking INSIDE the modal
+     //We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+   }
+  });
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+       return {
         add: add,
         getAll: getAll,
-        addListItem: addListItem
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails
       }})
     ();
   
-    pokemonRepository.add({name:"Bulbasaur",height:7,type:["grass","poison"]});
-    pokemonRepository.add({name:"Eve",height:22,type:["water","speed"]});
-    pokemonRepository.add({name:"Idunno", height:99,type:["gotme","gotyou"]});
-    // I am not famliar with Pokeman so I just made up names and characteristics>
-  
-  let pokemonListExternal = [];
-  pokemonListExternal = pokemonRepository.getAll();
-  
-  /*OLD CODE:
-  for (let i = 0; i < pokemonList.length; i++) {
-      console.log(pokemonList[i].name + " height = " +pokemonList[i].height);
-      document.write(pokemonList[i].name + " height = " +pokemonList[i].height);
-      if (pokemonList[i].height > 30)
-      {document.write(" - Wow, that's big!")};
-      document.write("<br>")  */
-  
-  //pokemonListExternal.forEach(function(user) {
-      //  pokemonListExternal is interchangeable in the forEach loop with pokemonRepository.gatAll().  Use pokemonRepository.getAll().
+  pokemonRepository.loadList().then(function() {
+      // Now the data is loaded!
   pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
-  //let pokemonList = document.querySelector('.pokemon-list'); 
-  //let listItem = document.createElement('li');
-  //let button = document.createElement('button');
-  //button.innerText = user.name;
-  //button.classList.add('buttonItem');
-  //listItem.appendChild(button);
-  //pokemonList.appendChild(listItem);
+    });
+
   });
-  
-  console.log(pokemonListExternal);
-    // added a list of Pokemon objects to the DOM along with their height
-  // included a line break for visual appearance
-  // added a condition to check for height above 30 and print a message to the DOM
